49bb10d773d7e571eb8f2e142b0e20d3823a6465
Aaron McGowan <47078754+HellfunDigital@users.noreply.github.com>
2023-01-24 21:52:48 +1100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -96,7 +96,20 @@ loginForm.addEventListener("submit", (e) => {
     });
 });
 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
 
 
