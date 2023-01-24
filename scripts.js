f614f40742794162c7bae07d0b3870e69e4dde74
Aaron McGowan <47078754+HellfunDigital@users.noreply.github.com>
2023-01-24 13:35:05 +1100

    firebase

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  databaseURL: "DATABASE_URL",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
 $(document).ready(function(){
     console.log("script is running");
     $("a").click(function(){
